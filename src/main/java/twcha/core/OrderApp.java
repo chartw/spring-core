c0e32bce92f3e1ca184f329541093a28288d3b18
chartw <12161800@inha.edu>
2022-09-12 18:06:11 +0900

    스프링으로 전환하기 ApplicationContext : 스프링 컨테이너 AppConfig로 직접 DI -> 스프링 컨테이너를 통해 DI
    스프링 컨테이너
    @Configuration이 붙은 Appconfig를 설정 정보로 사용
    @Bean이 붙은 메서드를 모두 호출하여 반환된 객체를 스프링 컨테이너에 등록
    이렇게 컨테이너에 등록된 객체 = 스프링 빈
    스프링 빈은 메서드 이름을 키값, 구현체를 value값으로 가짐
    
    기존 방식과의 차이와 스프링 컨테이너의 장점?

--- src/main/java/twcha/core/OrderApp.java
+++ src/main/java/twcha/core/OrderApp.java
@@ -1,5 +1,7 @@
 package twcha.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import twcha.core.member.Grade;
 import twcha.core.member.Member;
 import twcha.core.member.MemberService;
@@ -9,9 +11,13 @@ import twcha.core.order.OrderService;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
