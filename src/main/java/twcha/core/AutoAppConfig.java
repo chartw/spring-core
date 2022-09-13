872be925da039a0cd7555c69813f85ea35f4fe83
chartw <12161800@inha.edu>
2022-09-13 22:40:06 +0900

    탐색 위치와 기본 스캔 대상
    설정 정보 클래스의 위치를 프로젝트 최상단에 두고 basePackages 지정 생략
    -> 프로젝트 최상단이 시작 위치
    
    컴포넌트 스캔 대상
    @Component : 컴포넌트 스캔에서 사용
    @Controller : 스프링 MVC 컨트롤러로 인식
    @Repository : 스프링 데이터 접근 계층으로 인식하고, 데이터 계층의 예외를 스프링 예외로 변환해준다.
    @Configuration : 스프링 설정 정보로 인식하고, 스프링 빈이 싱글톤을 유지하도록 추가 처리를 한다.
    @Service : 특별한 처리를 하지 않는다. 핵심 비즈니스 로직이 여기에 있겠구나 라고 비즈니스 계층을 인식하는데 도움
    
    useDefaultFilters: 디폴트로 켜져있는데, 끄면 기본 스캔 대상들이 제외됨

--- src/main/java/twcha/core/AutoAppConfig.java
+++ src/main/java/twcha/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //base 지정안했을때 Default: @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치
+        //컴포넌트 스캔 대상 지정, {}로 다수 지정 가능
+        basePackages = "twcha.core.member",
+        //지정한 클래스의 패키지를 탐색 시작위치로 지정(twcha.core)
+        basePackageClasses = AutoAppConfig.class,
         //@Configuration이 붙은 AppConfig도 스프링빈에 등록되어 기존의 수동등록하는것과 달라지지 않음.
         //예제코드를 유지하기 위해 제외
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
--- src/main/java/twcha/core/CoreApplication.java
+++ src/main/java/twcha/core/CoreApplication.java
@@ -3,6 +3,7 @@ package twcha.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//해당 어노테이션에 @ComponentScan이 포함되어 있다. 때문에 자동으로 컴포넌트 스캔이 일어남.
 @SpringBootApplication
 public class CoreApplication {
 
